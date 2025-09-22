4aade28506a309f79c801aea3d85df9d8aa5fe61
Linxuan Jia <2559346258@qq.com>
2025-09-21 19:14:08 -0700

    init

---
+++ src/main/java/com/lucasjia/contactbookbackend/LucasJiaContactBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lucasjia.contactbookbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasJiaContactBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LucasJiaContactBookBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasjia/contactbookbackend/LucasJiaContactBookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasjia.contactbookbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasJiaContactBookBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
